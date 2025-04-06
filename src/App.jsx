a467e136357810f71d85141b00a43d26fa24b74a
Bryan1675 <bosseb6@gmail.com>
2025-04-06 23:04:23 +0200

    fix import error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home";
-import FicheProjet from "./pages/ficheProjet";
+import FicheProjet from "./pages/FicheProjet";
 
 
 function App() {
